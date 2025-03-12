0c84d5c338a293b0234302eaf46015228a1e242f
Chris How <chris@primesolid.com>
2025-03-12 09:04:46 +0100

    Initial commit - boilerplate

---
+++ dist/scroll-scrub-video-component.d.ts
@@ -0,0 +1,13 @@
+export declare class ScrollScrubVideoComponent extends HTMLElement {
+    constructor();
+    get greeting(): string;
+    set greeting(newValue: string);
+    static get observedAttributes(): string[];
+    attributeChangedCallback(property: string, oldValue: string, newValue: string): void;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        'scroll-scrub-video': ScrollScrubVideoComponent;
+    }
+}
+//# sourceMappingURL=scroll-scrub-video-component.d.ts.map
